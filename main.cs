ba663504da5876f8b9dcbbc4694ebe270de5c21d
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-13 17:56:09 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -11,20 +11,19 @@ class Program
         //Explanation of the algorithm
         Console.WriteLine("This program will count the number of digits in positve numbers(excluding decimals. It also excludes numbers with digits bigger than 19 due to a technicallity with c#). If zero starts your number it does not count as a digit.");
         Console.WriteLine("");
-        
         //Input for user’s number
         Console.WriteLine("Please enter your number");
         decimal number = Convert.ToInt64(Console.ReadLine());
-        Console.WriteLine(""); 
+        Console.WriteLine("");
         int counter = 0;
-        
         //Process: Function to check the amount of digits in any postive whole number(except 1)
         if  (number > 0) 
         {
-            while (number >= 1) {
+            while (number >= 1)
+            {
                 number = number / 10;
                 counter = counter + 1;
-                }
+            }
         }
         Console.WriteLine(counter);
         Console.WriteLine("\nDone");
